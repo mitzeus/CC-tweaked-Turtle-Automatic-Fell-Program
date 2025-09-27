3d291be22ee1ebe59e9b9510f5cd872c92967077
mitzeus_win_pc <91803944+mitzeus@users.noreply.github.com>
2025-09-27 21:32:21 +0200

    Major bug fixing, added SafeDig and hero added
    * Fixed a bug where turtle would run out of fuel and would do all instructions on the same block
    * Fixed a bug where turtle would suck items from output chest (back one) when returning from a round and then drop it into the chest again
    * Fixed a bug where turtle would take too many saplings from saplings chest and put it in the output chest instead.
    * Added a SafeDig toggle to more easily let turtle dig blocks in the way of its path

--- fell.lua
+++ fell.lua
@@ -10,7 +10,10 @@ coal = 1
 sapling = 2
 boneMeal = 3
 
+
+local MINFUEL = 50
 local SPEAK = true
+local SAFEDIG = true
 
 movementStack = {}
 
@@ -23,6 +26,28 @@ function say(msg)
     end
 end
 
+
+
+
+function forwardCheckMove() 
+    ensureFuel(MINFUEL)
+    if checkIfSpotEmpty() == false then
+        if SAFEDIG == true then
+            print("Path obstructed. Waiting for manual clearance..")
+            while checkIfSpotEmpty() == false do
+                sleep(5)
+            end
+            print("Path cleared. Continuing..")
+        elseif SAFEDIG == false then
+            print("Path obstructed. Safedig protocol off. Digging..")
+            while checkIfSpotEmpty() == false do
+                turtle.dig()
+            end
+        end
+    end
+    turtle.forward()
+end
+
 function clearTerminal()
     term.clear()
     term.setCursorPos(1,1)
@@ -31,6 +56,7 @@ end
 clearTerminal()
 
 print("Hi and welcome to Woot's Fell program!")
+print("\n")
 print("To get started, you'll need to provide Horizontal and Vertical Size.")
 print("Horizontal size: How many sets of forward and backward passes. 1 Horizontal = 6 blocks width.")
 print("Vertical size: How many blocks forward. 1 Vertical = 1 blocks depth")
@@ -38,11 +64,9 @@ print("Vertical size: How many blocks forward. 1 Vertical = 1 blocks depth")
 while true do 
     write("Continue> ") 
     read()
-    term.clear()
-term.setCursorPos(1,1)
+    clearTerminal()
     break
 end
-
 
 
 print("Do you want to enable Verbosity? (y/n) (default: y)")
@@ -59,6 +83,23 @@ while true do
     break
 end
 
+print("\n")
+print("Do you want to enable SafeDig? (y/n) (default and recommended: y)")
+print("SafeDig enabled will make sure that turtle does not dig unauthorized blocks. If disabled, turtle may BREAK UNWANTED BLOCKS if it gets lost.")
+while true do
+    write("Answer (y/n)> ")
+    local text = read()
+
+    if text == "n" or text == "no" or text == "nope" or text == "nah" then
+        SAFEDIG = false
+        print("SafeDig set to: FALSE")
+    else
+        print("SafeDig set to: TRUE")
+    end
+    break
+end
+
+print("\n")
 -- Asks for working area
 while true do
     write("Horizontal Size> ")
@@ -104,12 +145,37 @@ print("Beginning..")
 
 
 
+function ensureFuel(min)
+    min = min or 1
+    if turtle.getFuelLevel() < min then
+        refuel()
+    end
+end
+
 function refuel()
-    if turtle.getFuelLevel() < 51 then
+
         say("Refueling with current fuel..")
         turtle.select(coal)
+        currentFuelLevel = turtle.getFuelLevel()
         turtle.refuel(5)
-    end
+        newFuelLevel = turtle.getFuelLevel()
+
+        if newFuelLevel > currentFuelLevel then
+        else
+            print("Invalid or little to no fuel left. Please refill the turtle directly to continue..")
+            while true do
+                sleep(5)
+
+                currentFuelLevel = turtle.getFuelLevel()
+                turtle.refuel(5)
+                newFuelLevel = turtle.getFuelLevel()
+
+                if newFuelLevel > currentFuelLevel then
+                    print("Successfully refueled. Continuing..")
+                    break
+                end
+            end
+        end
 end
 
 
@@ -118,16 +184,37 @@ function refillEmpty()
     -- assuming default position
     -- coal
     turtle.turnLeft()
-    turtle.select(coal)
-    coalLeft = turtle.getItemCount(coal)
-
-    if coalLeft == 0 and turtle.suck() == false then
+    coalDetails = turtle.getItemDetail(coal)
+    if coalDetails == nil then
+        turtle.select(coal)
         print("Waiting for Coal to be Put in Chest..")
-        while not turtle.suck() do
+        while true do
+            turtle.suck()
+            if turtle.getItemCount(coal) > 0 then
+                break
+            end
+            
             sleep(5)
         end
+        -- sleep(5)
         print("Coal provided. Continuing..")
     end
+
+    -- turtle.select(coal)
+    -- coalLeft = turtle.getItemCount(coal)
+    -- -- turtle.suck(64 - coalLeft)
+
+    -- if coalLeft == 0 then
+    --     print("Waiting for Coal to be Put in Chest..")
+    --     while true do
+    --         local amount = turtle.getItemCount(coal)
+    --         if amount > 0 or turtle.suck() == true then
+    --             break
+    --         end
+    --         sleep(5)
+    --     end
+    --     print("Coal provided. Continuing..")
+    -- end
 
     turtle.turnRight()
     
@@ -139,8 +226,12 @@ function refillEmpty()
         turtle.select(sapling)
 
         print("Waiting for Saplings to be Put in Chest.. (Minimum: 2, Recommended: 64+)")
-        while turtle.getItemCount(sapling) < 2 do
+        while true do
             turtle.suck()
+            if turtle.getItemCount(sapling) > 1 then
+                break
+            end
+            
             sleep(5)
         end
         print("Saplings provided. Continuing..")
@@ -161,7 +252,8 @@ function refillEmpty()
         ::continue::
     end
     turtle.select(sapling)
-    turtle.suck()
+    saplingsLeft = turtle.getItemCount(sapling)
+    turtle.suck(64 - saplingsLeft)
     turtle.turnLeft()
     
      -- drop wood
@@ -178,11 +270,11 @@ end
 function fell()
     say("Felling..")
     turtle.dig()
-    turtle.forward()
+    forwardCheckMove()
     column()
     turtle.turnRight()
     turtle.turnRight()
-    turtle.forward()
+    forwardCheckMove()
     turtle.turnRight()
     turtle.turnRight()
 end
@@ -191,6 +283,7 @@ function column()
     local heightReached = 0
 
     while turtle.detectUp() == true do
+        ensureFuel(MINFUEL)
         turtle.digUp()
         turtle.up()
         heightReached = heightReached + 1
@@ -202,6 +295,7 @@ function column()
     -- end
 
     for j = 1,heightReached - 1 do
+        ensureFuel(MINFUEL)
         turtle.digDown()
         turtle.down()
         turtle.suck()
@@ -318,7 +412,7 @@ function forwardPass()
         if checkIfSpotEmpty() == false then
             print("TRYING TO FORWARD WHEN BLOCK BLOCKS.")
         end
-        turtle.forward()
+        forwardCheckMove()
         turtle.suck()
         table.insert(movementStack, "forward")
         checkAroundForTrees()
@@ -328,14 +422,14 @@ function forwardPass()
 
     -- pass done, turn for backwards pass
     turtle.suck()
-    turtle.forward()
+    forwardCheckMove()
     turtle.turnRight()
     turtle.suck()
-    turtle.forward()
+    forwardCheckMove()
     turtle.suck()
-    turtle.forward()
+    forwardCheckMove()
     turtle.suck()
-    turtle.forward()
+    forwardCheckMove()
     turtle.suck()
     turtle.turnRight()
 
@@ -353,7 +447,7 @@ function backwardPass()
         if checkIfSpotEmpty() == false then
             print("TRYING TO FORWARD WHEN BLOCK BLOCKS.")
         end
-        turtle.forward()
+        forwardCheckMove()
         turtle.suck()
         table.insert(movementStack, "forward")
         checkAroundForTrees()
@@ -361,14 +455,14 @@ function backwardPass()
 
     -- pass done, turn for backwards pass
     turtle.suck()
-    turtle.forward()
+    forwardCheckMove()
     turtle.turnLeft()
     turtle.suck()
-    turtle.forward()
+    forwardCheckMove()
     turtle.suck()
-    turtle.forward()
+    forwardCheckMove()
     turtle.suck()
-    turtle.forward()
+    forwardCheckMove()
     turtle.suck()
     turtle.turnLeft()
 
@@ -385,10 +479,10 @@ function returnHome()
     say("Returning Home..")
     turtle.turnRight()
     turtle.turnRight()
-    for j = #movementStack, 1, -1 do
+    for j = #movementStack, 2, -1 do
         -- inverse all instructions to go back
         if movementStack[j] == "forward" then
-            turtle.forward()
+            forwardCheckMove()
             turtle.suck()
         elseif movementStack[j] == "left" then
             turtle.turnRight()
@@ -400,6 +494,9 @@ function returnHome()
             print("Invalid movement, must be forward, left or right")
         end
     end
+
+    -- as for loop is only to 2 and needs to get into default position. Prevents sucking from back chest
+    forwardCheckMove()
 
     turtle.turnLeft()
     turtle.turnLeft()
@@ -431,8 +528,8 @@ end
 repeat
     
     -- searching work
-    refuel()
     refillEmpty()
+    ensureFuel(MINFUEL)
 
     for j = 1,horizontalSize do
         forwardPass()
